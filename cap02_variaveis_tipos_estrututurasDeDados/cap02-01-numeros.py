15d5de525672f572295010090495bbce86e004e0
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-07 17:09:01 -0300

    Update cap02-01-numeros.py

--- cap02_variaveis_tipos_estrututurasDeDados/cap02-01-numeros.py
+++ cap02_variaveis_tipos_estrututurasDeDados/cap02-01-numeros.py
@@ -1,216 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # <font color='blue'>Data Science Academy - Python Fundamentos - Capítulo 2</font>
-# 
-# ## Download: http://github.com/dsacademybr
-
-# In[1]:
-
-
-# Versão da Linguagem Python
-from platform import python_version
-print('Versão da Linguagem Python Usada Neste Jupyter Notebook:', python_version())
-
-
-# ## Números e Operações Matemáticas
-
-# # Pressione as teclas shift e enter para executar o código em uma célula ou pressione o botão Play no menu superior
-
-# In[2]:
-
-
 # Soma
 4 + 4
-
-
-# In[3]:
-
 
 # Subtração
 4 - 3
 
-
-# In[4]:
-
-
 # Multiplicação
 3 * 3
-
-
-# In[5]:
-
 
 # Divisão
 3 / 2
 
-
-# In[6]:
-
-
 # Potência
 4 ** 2  
-
-
-# In[7]:
-
 
 # Módulo
 10 % 3  
 
 
 # ### Função Type
-
-# In[8]:
-
-
 type(5)
-
-
-# In[9]:
-
-
 type(5.0)
-
-
-# In[10]:
-
-
 a = 'Eu sou uma string'
 type(a)
 
 
 # ### Operações com números float
-
-# In[11]:
-
-
 3.1 + 6.4
-
-
-# In[12]:
-
-
 4 + 4.0
-
-
-# In[13]:
-
-
 4 + 4
-
-
-# In[14]:
-
+4 / 3.0
 
 # Resultado é um número float
 4 / 2
 
-
-# In[15]:
-
-
 # Resultado é um número inteiro
 4 // 2
-
-
-# In[16]:
-
-
-4 / 3.0
-
-
-# In[17]:
-
-
 4 // 3.0
 
 
 # ### Conversão
-
-# In[18]:
-
-
 float(9)
-
-
-# In[19]:
-
-
 int(6.0)
-
-
-# In[20]:
-
-
 int(6.5)
 
 
 # ### Hexadecimal e Binário
-
-# In[21]:
-
-
 hex(394)
-
-
-# In[22]:
-
-
 hex(217)
-
-
-# In[23]:
-
-
 bin(286)
-
-
-# In[24]:
-
-
 bin(390)
-
-
-# ### Funções abs, round e pow
-
-# In[25]:
-
 
 # Retorna o valor absoluto
 abs(-8)
-
-
-# In[26]:
-
-
-# Retorna o valor absoluto
 abs(8)
-
-
-# In[27]:
-
 
 # Retorna o valor com arredondamento
 round(3.14151922,2)
 
-
-# In[28]:
-
-
 # Potência
 pow(4,2)
-
-
-# In[29]:
-
-
-# Potência
 pow(5,3)
-
-
-# # Fim
-
-# ### Obrigado
-# 
-# ### Visite o Blog da Data Science Academy - <a href="http://blog.dsacademy.com.br">Blog DSA</a>

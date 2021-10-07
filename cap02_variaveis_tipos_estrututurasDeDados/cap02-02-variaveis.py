fd740d126936a943b46fbe52162328008f00ebaf
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-07 18:17:20 -0300

    Create cap02-02-variaveis.py

---
+++ cap02_variaveis_tipos_estrututurasDeDados/cap02-02-variaveis.py
@@ -0,0 +1,57 @@
+# Atribuir o valor 1 à variável var_teste
+var_teste = 1
+
+# Imprimir o valor da variável
+print(var_teste)
+
+# Atribuir o valor 2 à variável var_teste
+var_teste = 2
+
+# Imprimir o valor da variável
+print(var_teste)
+
+# Exibir o tipo de dados da variável
+type(var_teste)
+
+# Atribuir o valor 9.5 à variável var_teste
+var_teste = 9.5
+
+# Exibir o tipo de dados da variável
+type(var_teste)
+
+# ## Declaração Múltipla
+pessoa1, pessoa2, pessoa3 = "Maria", "José", "Tobias"
+fruta1 = fruta2 = fruta3 = "Laranja"
+
+# ## Pode-se usar letras, números e underline (mas não se pode começar com números)
+x1 = 50
+
+
+# ## Variáveis atribuídas a outras variáveis e ordem dos operadores
+largura = 2
+altura = 4
+
+area = largura * altura
+print(area)
+
+perimetro = 2 * largura + 2 * altura
+print(perimetro)
+
+perimetro = 2 * (largura + 2)  * altura
+print(perimetro)
+
+idade1 = 25
+idade2 = 35
+
+idade1 + idade2
+idade2 - idade1
+idade2 * idade1
+idade2 / idade1
+idade2 % idade1
+
+
+# ## Concatenação de Variáveis
+nome = "Steve"
+sobrenome = "Jobs"
+fullName = nome + " " + sobrenome
+print(fullName)

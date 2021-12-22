6eb29730bba29ef7231ced5819b62b0d79ab4450
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-12-21 21:31:49 -0300

    Create cap02-06-tuplas.py

---
+++ cap02_variaveis_tipos_estrututurasDeDados/cap02-06-tuplas.py
@@ -0,0 +1,14 @@
+# Tuplas são declaradas com () e são imutáveis
+tupla = (1, 2, 3, 4, 5)
+
+# Verificar o comprimento da tupla
+len(tupla)
+
+# Slicing
+tupla[1:]
+
+# Verificar o índice de um elemento
+tupla.index(3)
+
+# Excluir a tupla
+del tupla
